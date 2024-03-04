5548125364807bef4d39d44341e9d9c744a0a768
tanushree1810 <tkaushik901@gmail.com>
2024-03-05 01:45:01 +0530

    files

---
+++ react-notes-app/src/components/AddNote.js
@@ -0,0 +1,37 @@
+import {useState} from 'react';
+
+const AddNotes=({handleAddNote})=>{
+    const[noteText,setNoteText]=useState('');
+    const characteLimit=200;
+    const handleChange=(event)=>{
+            if(characteLimit-event.target.value.length>=0){
+                setNoteText(event.target.value);
+            }
+                
+    };
+
+    const handleSaveClick=()=>{
+        if(noteText.trim().length>0){
+            handleAddNote(noteText);
+            setNoteText('');
+        }
+        
+    };
+
+    return(<div className="note new">
+        <textarea 
+            rows='8'
+            cols='10'
+            placeholder="Type to add a Note.."
+            value={noteText}
+            onChange={handleChange}
+        ></textarea>
+        <div className="note-footer">
+            <small>{characteLimit-noteText.length} Remaining</small>
+            <button className="save" onClick={handleSaveClick}>Save</button>
+        </div>
+
+    </div>);
+};
+
+export default AddNotes;
